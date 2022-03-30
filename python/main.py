4d24530c44f598e24fd169d723f54065dcec84b5
Колесник Мария <kolesnik.mariya@corp.wildberries.ru>
2022-03-30 22:27:58 +0300

    Fixed uniqueness error

--- python/main.py
+++ python/main.py
@@ -5,13 +5,11 @@ import random as rd
 def generateUniqueNumbers(n: int) -> List[int]:
     left_border = int(input("From which number should we start? \n"))
     right_border = int(input("Which number is the end of the range? \n"))
-    random_list = []
-    for i in range(n):
-        if i not in random_list:
-            random_list.append(rd.randint(left_border, right_border))
-        pass
+    result_set = set()
+    while len(result_set) < n:
+        result_set.add(rd.randint(0, n*2))
 
-    return random_list
+    return list(result_set)
 
 
 print(generateUniqueNumbers(5))
